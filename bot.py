7316c8a7548a8b984a27e05a87abb57a0d45d4d8
gamerzocker69-wq <gamerzocker69@yahoo.fr>
2026-03-10 12:38:27 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,7 @@
 import os
 TOKEN = os.getenv('8713779724:AAFJ3LZvGSqHfZp5vw_v4mb3c7olR5WshVw')
 if not TOKEN:
+    print("ERREUR : Aucune variable d'environnement nommée 'TOKEN' trouvée")
     raise ValueError("TOKEN manquant ! Ajoute-le dans les variables Railway.")
+else:
+    print(f"TOKEN chargé avec succès (longueur {len(TOKEN)} caractères)")
